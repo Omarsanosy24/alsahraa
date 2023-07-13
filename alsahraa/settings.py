f4e515989cb2e5ded5b618ff025b8750d15b80dc
Omarsanosy24 <113290018+Omarsanosy24@users.noreply.github.com>
2023-07-13 04:13:07 +0300

    tette

--- alsahraa/settings.py
+++ alsahraa/settings.py
@@ -112,7 +112,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50,
+    'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',),
      'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
