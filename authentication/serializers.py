08df1c5ca79413d2a933dbbc395c12eeddde2057
Omarsanosy24 <113290018+Omarsanosy24@users.noreply.github.com>
2023-07-20 23:52:10 +0300

    deploy

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -161,7 +161,7 @@ class LogoutSerializer(serializers.Serializer):
 class UserSer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username','phone','is_staff','email']
+        fields = ['username','phone','kind','email']
 
 class TokenSerializers(serializers.ModelSerializer):
     user= UserSer(read_only=True)
