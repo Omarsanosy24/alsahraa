f752e76962e1219c1956857e11e2f5296c74fc9d
Omarsanosy24 <113290018+Omarsanosy24@users.noreply.github.com>
2023-08-06 16:05:20 +0300

    Update serializers.py
    deploy

--- proudects/serializers.py
+++ proudects/serializers.py
@@ -38,11 +38,12 @@ class CategorySerializers(serializers.ModelSerializer):
         sub = validated_data.pop('subCat', None)
         name_ar = validated_data.pop('name_ar', None)
         name_en = validated_data.pop('name_en', None)
-        if Category.objects.get(id=sub).subCategory:
-            raise serializers.ValidationError({
-                "sub":"لا يمكنك استخدام هذا المسار"
-            })
-        if mainCategory and sub:
+        if sub != None:
+            if Category.objects.get(id=sub).subCategory:
+                raise serializers.ValidationError({
+                    "sub":"لا يمكنك استخدام هذا المسار"
+                })
+        if mainCategory != None and sub != None:
             raise serializers.ValidationError({
                 "sub":"لا يمكنك اختيار فرع رئيسي وفرع ثانوي معاً"
             })
