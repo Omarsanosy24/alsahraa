a5515189cb7d70a9b691a4ca9a3a58d6bbe13596
Omarsanosy24 <113290018+Omarsanosy24@users.noreply.github.com>
2024-01-12 23:16:37 +0200

    payment and deploy

--- payment/views.py
+++ payment/views.py
@@ -142,7 +142,7 @@ def pay(order):
             }
         },
         "source": { "id": "src_card" },
-        "post": { "url": "https://aoi.alsahraa-abs.com/payment/data" },
+        "post": { "url": "https://api.alsahraa-abs.com/payment/data" },
         "redirect": { "url": "https://alsahraa-abs.com/en/cart" }
     }
     headers = {
