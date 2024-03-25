c50e3b07cffe2be7fe1490dbeb559f67723c5120
Omarsanosy24 <113290018+Omarsanosy24@users.noreply.github.com>
2024-03-25 22:00:37 +0200

    add links to admin and deploy

--- proudects/models.py
+++ proudects/models.py
@@ -161,7 +161,7 @@ class Links(models.Model):
         ('twitter',"twitter"),
     ]
     kind = models.CharField(choices=choices, max_length=20, unique=True)
-    value = models.CharField(max_length=50)
+    value = models.TextField()
     
     def __str__(self) -> str:
         return self.kind
